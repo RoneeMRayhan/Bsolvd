72ea389f91a978ca4ca330355d1709a649e1ee2d
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2020-07-22 09:43:03 +0600

    FunctionalCurved NavBar

--- lib/FbStorage/BottomNavigationBar.dart
+++ lib/FbStorage/BottomNavigationBar.dart
@@ -1,3 +1,4 @@
+import 'package:bsolvd/FbStorage/RayStoryView.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
@@ -12,7 +13,7 @@ class NavigationBar extends StatefulWidget {
 class _NavigationBarState extends State<NavigationBar> {
   int _currentIndex = 0;
   GlobalKey _bottomNavigationKey = GlobalKey();
-  PageController _pageController;
+  PageController _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
@@ -56,6 +57,20 @@ class _NavigationBarState extends State<NavigationBar> {
             _pageController.jumpToPage(index);
           });
         },
+      ),
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (value) {
+            setState(() => _currentIndex = value);
+          },
+          children: <Widget>[
+            
+            RayStoryView(),
+            RayStoryView(),
+            RayStoryView(),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/FbStorage/RayStoryView.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RayStoryView extends StatefulWidget {
+  RayStoryView({Key key}) : super(key: key);
+
+  @override
+  _RayStoryViewState createState() => _RayStoryViewState();
+}
+
+class _RayStoryViewState extends State<RayStoryView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
