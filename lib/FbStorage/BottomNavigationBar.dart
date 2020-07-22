602182d5649b7c822fe45331f5e9cc5bc798d198
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2020-07-22 09:14:50 +0600

    Curved NavBar added

--- lib/FbStorage/BottomNavigationBar.dart
+++ lib/FbStorage/BottomNavigationBar.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
 
@@ -9,21 +10,53 @@ class NavigationBar extends StatefulWidget {
 }
 
 class _NavigationBarState extends State<NavigationBar> {
+  int _currentIndex = 0;
+  GlobalKey _bottomNavigationKey = GlobalKey();
+  PageController _pageController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: GradientAppBar(
-         title: Text(
-           "Video"
-         ),
-         flexibleSpace: Container(
-           decoration: BoxDecoration(
-             gradient: LinearGradient(
-               colors: [Color(0xee33ff00), Color(0xff99ff00)], stops: [0.5,1.0],
-             )
-           ),
-         ),
-       ),
+      appBar: GradientAppBar(
+        title: Text("Video"),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [Color(0xee33ff00), Color(0xff99ff00)],
+            stops: [0.5, 1.0],
+          )),
+        ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        height: 55,
+        index: _currentIndex,
+        color: Colors.amber,
+        backgroundColor: Colors.grey,
+        key: _bottomNavigationKey,
+        items: <Widget>[
+          Icon(
+            Icons.image,
+            size: 30,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.video_library,
+            size: 30,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.compare_arrows,
+            size: 30,
+            color: Colors.white,
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+            _pageController.jumpToPage(index);
+          });
+        },
+      ),
     );
   }
 }
