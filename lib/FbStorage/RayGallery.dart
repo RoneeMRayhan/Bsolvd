9b4a427a30909617a2a3d37302379775a331ebd2
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2020-07-23 19:54:09 +0600

    Gallery implemented

--- lib/FbStorage/RayGallery.dart
+++ lib/FbStorage/RayGallery.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:sticky_headers/sticky_headers.dart';
+import 'package:flutter_widget_gallery/gallery/gallery.dart';
 
 class RayGallery extends StatefulWidget {
   RayGallery({Key key}) : super(key: key);
@@ -12,7 +13,7 @@ class RayGallery extends StatefulWidget {
 class _RayGalleryState extends State<RayGallery> {
   final fdb = FirebaseDatabase.instance.reference().child("RayImages");
   List<String> itemList = new List();
-  List<String> listHeader = ["Gallery"];
+  List<String> listHeader = ["Images", "Videos"];
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
@@ -23,58 +24,79 @@ class _RayGalleryState extends State<RayGallery> {
             : Container(
                 height: height - 380,
                 child: ListView.builder(
-                    itemCount: listHeader.length,
-                    itemBuilder: (context, index) {
-                      return new StickyHeader(
-                          header: new Container(
-                            height: 38.0,
-                            color: Colors.transparent,
-                            padding: new EdgeInsets.symmetric(horizontal: 12.0),
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              listHeader[index],
-                              style: const TextStyle(
-                                  color: Colors.purple,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold),
+                  itemCount: listHeader.length,
+                  itemBuilder: (context, index) {
+                    return new StickyHeader(
+                      header: new Container(
+                        height: 38.0,
+                        color: Colors.transparent,
+                        padding: new EdgeInsets.symmetric(horizontal: 12.0),
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          listHeader[index],
+                          style: const TextStyle(
+                              color: Colors.purple,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      content: Container(
+                        color: Colors.white,
+                        child: Card(
+                          color: Colors.white,
+                          child: GridView.builder(
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            itemCount: itemList.length,
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 4,
+                              childAspectRatio: 1,
                             ),
-                          ),
-                          content: Container(
-                            color: Colors.white,
-                            child: Card(
-                              color: Colors.white,
-                              child: GridView.builder(
-                                  shrinkWrap: true,
-                                  physics: NeverScrollableScrollPhysics(),
-                                  itemCount: itemList.length,
-                                  gridDelegate:
-                                      SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 4,
-                                    childAspectRatio: 1,
-                                  ),
-                                  itemBuilder: (contxt, indx) {
-                                    return GestureDetector(
-                                      //onTap: ,
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(4),
-                                        child: Container(
-                                          height: 100,
-                                          width: 100,
-                                          decoration: BoxDecoration(
-                                            image: DecorationImage(
-                                              image: NetworkImage(
-                                                itemList[indx],
-                                              ),
-                                            ),
-                                          ),
+                            itemBuilder: (contxt, indx) {
+                              return GestureDetector(
+                                //onTap: ,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(4),
+                                  child: Container(
+                                    height: 100,
+                                    width: 100,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        image: NetworkImage(
+                                          itemList[indx],
                                         ),
                                       ),
-                                    );
-                                  }),
-                            ),
-                          ));
-                    }),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
+        Container(
+          height: 240,
+          child: Padding(
+            padding: const EdgeInsets.all(0),
+            child: Gallery(
+              activeItemColor: Color(0xFF6A0AEA),
+              backgroundColor: Color(0xFFF3F3F3),
+              carouselBackgroundColor: Colors.purple[100],
+              children: List<Widget>.generate(
+                10,
+                (index) => Image.network(
+                  'https://picsum.photos/960/540?image=$index',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
